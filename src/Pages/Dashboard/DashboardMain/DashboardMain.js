7202db13adb39e6a8863a4d35844dd1e6de85c13
Mahir-Asef <mahirasef30@gmail.com>
2022-01-12 23:23:58 +0600

    dashboard changed

--- src/Pages/Dashboard/DashboardMain/DashboardMain.js
+++ src/Pages/Dashboard/DashboardMain/DashboardMain.js
@@ -4,7 +4,7 @@ import './DashboardMain.css';
 const DashboardMain = () => {
     return (
         <div className="dashboardImage">  
-              <h3 className="text-center text-white" >welcome to Dashboard</h3>
+              <h3 className="text-center text-white" >welcome  to Bike zone Dashboard</h3>
         </div>
     );
 };
